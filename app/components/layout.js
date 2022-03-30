4e09fbcc051ce5148bdae6af26bd9b64481ee4e0
Alexander Mykolaichuk <alexmix3000@gmail.com>
2022-03-30 19:33:42 +0200

    s3 files save

--- app/components/layout.js
+++ app/components/layout.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
@@ -33,18 +32,6 @@ export default function Layout({ children, home }) {
           </>
         ) : (
           <>
-            <Link href="/">
-              <a>
-                <Image
-                  priority
-                  src="/images/profile.jpg"
-                  className={utilStyles.borderCircle}
-                  height={108}
-                  width={108}
-                  alt={name}
-                />
-              </a>
-            </Link>
             <h2 className={utilStyles.headingLg}>
               <Link href="/">
                 <a className={utilStyles.colorInherit}>{name}</a>
--- terraform/main.ts
+++ terraform/main.ts
@@ -1,4 +1,8 @@
 import { Construct } from "constructs";
+import glob from "glob";
+import path from "path";
+import mimeTypes from "mime-types";
+import crypto from "crypto";
 import { App, TerraformStack } from "cdktf";
 import { AwsProvider, s3 } from "@cdktf/provider-aws";
 
@@ -12,15 +16,29 @@ class AWSDeploymentPreviewStack extends TerraformStack {
       });
 
 
-    const bucket = new s3.S3Bucket(this, 'aws_s3_bucket', {
+    const bucket = new s3.S3Bucket(this, "aws_s3_bucket", {
         bucket: "alm-gg4h3ds4hs-bucket-test",
     });
 
-    new s3.S3BucketAcl(this, 'aws_s3_bucket_acl', {
-        bucket: bucket.id,
+    new s3.S3BucketAcl(this, "aws_s3_bucket_acl", {
+        bucket: bucket.bucket,
         acl: "private",
     });
 
+    const pattern = path.resolve(__dirname + "/../app/build");
+
+    const files = glob.sync(`${pattern}/**/*`, { absolute: false, nodir: true });
+
+    for (const file of files) {
+        new s3.S3Object(this, `aws_s3_object_${path.parse(file).name}`, {
+            bucket: bucket.bucket,
+            dependsOn: [bucket],
+            key: file.replace(`${pattern}/`, ""),
+            source: file,
+            etag: crypto.createHash("md5").update(file).digest("hex"),
+            contentType: mimeTypes.contentType(path.extname(file)) || ""
+        });
+    }
   }
 }
 
