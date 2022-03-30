402300262ce0cdad6a839c0bb8eaad6f1a076bd9
Alexander Mykolaichuk <alexmix3000@gmail.com>
2022-03-30 14:03:27 +0200

    nextjs sample app

---
+++ app/pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+    </Layout>
+  )
+}
