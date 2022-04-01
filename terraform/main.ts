80067ce3bb68d8ea1f2fb8ac150f0259412f70b7
Alexander Mykolaichuk <alexmix3000@gmail.com>
2022-04-01 13:42:41 +0200

    created wilcard domain

--- terraform/main.ts
+++ terraform/main.ts
@@ -4,13 +4,15 @@ import path from "path";
 import mimeTypes from "mime-types";
 import crypto from "crypto";
 import { App, TerraformStack } from "cdktf";
-import { AwsProvider, s3, cloudfront } from "@cdktf/provider-aws";
-
+import { AwsProvider, s3, cloudfront, acm, route53 } from "@cdktf/provider-aws";
 
 // AWS Resources are configured in one file for the demo purposes.
 
 const BUCKET_NAME = "aws-deployment-previews-root";
 const AWS_REGION = "us-east-1";
+
+const DOMAIN_NAME = "al-sandbox.com";
+const WILCARD_DOMAIN_NAME = `*.${DOMAIN_NAME}`;
 
 class AWSDeploymentPreviewStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
@@ -42,6 +44,32 @@ class AWSDeploymentPreviewStack extends TerraformStack {
 
     const GIT_BRANCH = "b3442af";
 
+    const acmCert = new acm.AcmCertificate(this, 'aws_acm_certificate', {
+        domainName: DOMAIN_NAME,
+        subjectAlternativeNames: [WILCARD_DOMAIN_NAME],
+        validationMethod: "DNS"
+    });
+
+    const zone = new route53.DataAwsRoute53Zone(this, 'aws_route53_zone', {
+        name: `${DOMAIN_NAME}.`,
+    });
+
+    const route53Record = new route53.Route53Record(this, 'aws_route53_record', {
+        name: acmCert.domainValidationOptions("0").resourceRecordName,
+        type: acmCert.domainValidationOptions("0").resourceRecordType,
+        records: [acmCert.domainValidationOptions("0").resourceRecordValue],
+        zoneId: zone.zoneId,
+        ttl: 60,
+        allowOverwrite: true,
+    });
+
+    new acm.AcmCertificateValidation(this, 'aws_acm_certificate_validation', {
+        certificateArn: acmCert.arn,
+        validationRecordFqdns: [
+            route53Record.fqdn
+        ]
+    });
+
     for (const file of files) {
         new s3.S3Object(this, `aws_s3_object_${path.parse(file).name}`, {
             bucket: bucket.bucket,
@@ -54,10 +82,11 @@ class AWSDeploymentPreviewStack extends TerraformStack {
         });
     }
 
-    new cloudfront.CloudfrontDistribution(this, "aws_cloudfront_distribution", {
+    const cloudfrontDistribution = new cloudfront.CloudfrontDistribution(this, "aws_cloudfront_distribution", {
         enabled: true,
         dependsOn: [bucket],
         isIpv6Enabled: true,
+        aliases: [WILCARD_DOMAIN_NAME],
         defaultRootObject: "index.html",
         origin: [
             {
@@ -89,9 +118,22 @@ class AWSDeploymentPreviewStack extends TerraformStack {
             }
         },
         viewerCertificate: {
-            cloudfrontDefaultCertificate: true,
+            acmCertificateArn: acmCert.arn,
+            sslSupportMethod: "sni-only",
+            minimumProtocolVersion: "TLSv1.1_2016"
         }
     })
+
+    new route53.Route53Record(this, 'aws_route53_record_wildcard', {
+        zoneId: zone.zoneId,
+        name: WILCARD_DOMAIN_NAME,
+        type: "A",
+        alias: [{
+            name: cloudfrontDistribution.domainName,
+            zoneId: cloudfrontDistribution.hostedZoneId,
+            evaluateTargetHealth: false,
+        }]
+    });
   }
 }
 
